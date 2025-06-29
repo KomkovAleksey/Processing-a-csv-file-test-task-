96c8c5423ee118ac1ab756c8af84d0a32375e67b
Алексей Комков <ale45883412@yandex.ru>
2025-06-29 17:02:21 +0700

    fix

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ def open_read_csv(csv_file_name: str) -> list:
     if os.path.exists(csv_file_name):
         with open(csv_file_name, encoding='utf-8') as csvfile:
             reader = csv.DictReader(csvfile)
-            data = list(reader)
-            return data
+            return list(reader)
     else:
         raise FileNotFoundError('csv file not found!')
