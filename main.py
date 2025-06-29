6693666a06f6f3e27dec570af59fba7e249bf9e7
Алексей Комков <ale45883412@yandex.ru>
2025-06-29 16:33:57 +0700

    removed the unnecessary variable

--- main.py
+++ main.py
@@ -4,9 +4,8 @@ import csv
 
 def open_read_csv(csv_file_name: str) -> list:
     """Open .csv file and return data as list of dictionaries."""
-    csv_file_path = csv_file_name
-    if os.path.exists(csv_file_path):
-        with open(csv_file_path, encoding='utf-8') as csvfile:
+    if os.path.exists(csv_file_name):
+        with open(csv_file_name, encoding='utf-8') as csvfile:
             reader = csv.DictReader(csvfile)
             data = list(reader)
             return data
